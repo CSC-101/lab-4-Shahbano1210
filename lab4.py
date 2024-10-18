b9cf96ba0e850e3e8b5a62024738c6f43b755457
Shahbano1210 <saurangz@calpoly.edu>
2024-10-18 11:39:53 -0700

    part 1 & 2

--- lab4.py
+++ lab4.py
@@ -3,10 +3,23 @@ import data
 # Write your functions for each part in the space below.
 
 # Part 1
-
+def first_element (list1:list[list[int]]) -> list: # takes in a list of nested lists of int and returns a list of just
+    # the first element of each nested list
+    list1 = [x for x in list1 if x != []]
+    list2 = []
+    for x in list1:
+        a = x[0]
+        list2.append(a)
+    return list2
 
 # Part 2
 
+def x_coordinates (list1:list[float,float]) -> list: # takes in a point made of an x and y value and returns the x value
+    list2=[]
+    for x in list1:
+        a = x[0]
+        list2.append(a)
+    return list2
 
 # Part 3
 
--- lab4_tests.py
+++ lab4_tests.py
@@ -16,10 +16,23 @@ class TestCases(unittest.TestCase):
 
     def test_first_element_2(self):
         # write a second test here
-
+        input = [[1,2],[5,6],[]]
+        result = lab4.first_element(input)
+        expected = [1,5]
+        self.assertEqual(expected, result)
 
     # Part 2
+    def test_x_coordinates_1(self):
+        input = [[1.0,2.0],[3.0,4.0]]
+        result = lab4.x_coordinates(input)
+        expected = [1.0,3.0]
+        self.assertEqual(expected, result)
 
+    def test_x_coordinates_2(self):
+        input = [[1.5,2.0],[1.5,4.0]]
+        result = lab4.x_coordinates(input)
+        expected = [1.5,1.5]
+        self.assertEqual(expected, result)
 
     # Part 3
 
